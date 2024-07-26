5804747d277fd358096a9371d1c4bc9ae68176c4
bopzen <boppp@abv.bg>
2024-07-26 16:06:45 +0300

    Updated Home component

--- front-end/src/components/home/Home.jsx
+++ front-end/src/components/home/Home.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
-            
+            {/* <h1>Home</h1> */}
+
+            <div className="home-background">
+                <img src="/images/lime-near-roasted-meat-salad.jpg" alt="" />
+
+                <div className="welcome-message">
+                    <h1>Welcome to </h1>
+                    <h1>Restaurant Finder</h1>
+                    <h3>Not sure where to eat today?</h3>
+                    <h3>We got you govered</h3>
+                    <h3>Choose the best restaurant around you</h3>
+                </div>
+                <div className="welcome-buttons">
+                    <Link className="welcome-btn" to="/restaurants">RESTAURANTS</Link>
+                    <Link className="welcome-btn" to="/map">RESTAURANTS MAP</Link>
+                </div>
+            </div>
         </>
     )
 }
