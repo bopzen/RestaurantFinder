7acd50f08a2d5e4c618123d895082bf8fb354bac
bopzen <boppp@abv.bg>
2024-08-07 12:41:07 +0300

    Updated RestaurantReviews component

--- front-end/src/components/reviews/RestaurantReviews.jsx
+++ front-end/src/components/reviews/RestaurantReviews.jsx
@@ -3,6 +3,7 @@ import { useApi } from "../../hooks/useApi";
 import { BASE_API_URL } from "../../constants/constants";
 import LoadingSpinner from "../loading-spinner/LoadingSpinner";
 import { useEffect, useState } from "react";
+import ShowStars from "./ShowStars";
 
 export default function RestaurantReviews() {
     const { restaurantId } = useParams();
@@ -84,20 +85,35 @@ export default function RestaurantReviews() {
         return <div>No reviews available</div>;
     }
     
+    const formatDate = (dateString) => {
+        let date = new Date(dateString);
+        const options = {
+            year: 'numeric',
+            month: 'long', 
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+    };
+    return date.toLocaleDateString('en-BG', options);
+    }
+
     
 
     return (
         <section className="restaurant-reviews">
             <h1>{restaurant.name} reviews</h1>
-            <ul>
+            <div className="restaurant-reviews-container">
                 {reviews.map((review) => (
-                    <li key={review._id}>
-                        <h4>Comment: {review.comment}</h4>
-                        <p>Rating: {review.rating}</p>
-                        <p>By: {review.email}</p>
-                    </li>
+                    <div className="restaurant-review-card" key={review._id}>
+                        <div>
+                            <b><p>{review.email}</p></b>
+                            <ShowStars rating={review.rating}/>
+                            <p>{formatDate(review._createdOn)}</p>
+                        </div>   
+                        <b><p>{review.comment}</p></b>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </section>
     )
 
