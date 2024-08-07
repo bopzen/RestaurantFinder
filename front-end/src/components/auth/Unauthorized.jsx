3403190b57d5a522439daea9e03c82002c58cefe
bopzen <boppp@abv.bg>
2024-08-07 16:04:06 +0300

    Updated not found and unauthorized components

--- front-end/src/components/auth/Unauthorized.jsx
+++ front-end/src/components/auth/Unauthorized.jsx
@@ -1,7 +1,12 @@
 export default function Unauthorized() {
     return (
-        <>
+            
+            <section className="not-found">
             <h1>You are not authorized!</h1>
-        </>
+            <div className="not-found-img">
+                <img src="/images/thom-masat-iA2f8YAOeOk-unsplash.jpg" alt="404" />
+            </div>
+        </section>
+
     )
 }
--- front-end/src/components/not-found/NotFound.jsx
+++ front-end/src/components/not-found/NotFound.jsx
@@ -1,7 +1,10 @@
 export default function NotFound() {
     return (
-        <>
+        <section className="not-found">
             <h1>404 Not Found</h1>
-        </>
+            <div className="not-found-img">
+                <img src="/images/richard-bell-NXHwphKj1Yc-unsplash.jpg" alt="404" />
+            </div>
+        </section>
     )
 }
